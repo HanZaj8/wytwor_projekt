b73851e6a2877fb3e27b76b39e77066961dd4e3a
HanZaj8 <hzajko@st.swps.edu.pl>
2025-05-22 21:39:04 +0200

    API KEY update chatbot_backend.py

--- projekt_wytwor/chatbot_backend.py
+++ projekt_wytwor/chatbot_backend.py
@@ -14,7 +14,7 @@ import pandas as pd
 from langchain_core.prompts import ChatPromptTemplate
 
 folder_path = os.getcwd()
-groq_key = "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"
+groq_key = "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"
 os.environ['GROQ_API_KEY'] = groq_key
 
 def setting_up_chatbot():
